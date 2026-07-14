9358ccdf15c9b0ebd4b975b45d087036e5dcd8e1
raghda-soliman-ece <raghad.soliman2022@gmail.com>
2026-07-14 15:53:54 +0300

    Delete Back end directory

--- Back end/DTOs/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Jumia.DTOs
-{
-    public class UserDto
-    {
-        public string DisplayName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Token { get; set; } = string.Empty;
-    }
-}
--- Back end/Services/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Jumia.Jumia.Domain.Models;
-
-namespace Jumia.Services
-{
-    public interface ITokenService
-    {
-        string CreateToken(AppUser user);
-    }
-}
